1c2f266e0a468aaa2ad0ca4ca252cf4f6d733fda
Steffen Elste <steffen.elste@gmail.com>
2017-04-17 12:48:22 +0200

    Fix - use internal oscillator

--- Libraries/Arduino/examples/SparkFunLibrarySimplifiedTest/SparkFunSupportFunctions.ino
+++ Libraries/Arduino/examples/SparkFunLibrarySimplifiedTest/SparkFunSupportFunctions.ino
@@ -23,30 +23,30 @@ void playNote(int note, int duration) {
 // This is the configuration function for the dSPIN parts. Read the inline
 //  comments for more info.
 void dSPINConfig(void) {
-  board.SPIPortConnect(&SPI);                // Before doing anything else, we need to
-                                             //  tell the objects which SPI port to use.
-                                             //  Some devices may have more than one.
+  board.SPIPortConnect(&SPI);             // Before doing anything else, we need to
+                                          //  tell the objects which SPI port to use.
+                                          //  Some devices may have more than one.
 
-  board.configSyncPin(BUSY_PIN, 0);          // BUSY pin low during operations;
-                                             //  second paramter ignored.
-  board.configStepMode(STEP_FS);             // 0 microsteps per step
-  board.setMaxSpeed(10000);                  // 10000 steps/s max
-  board.setFullSpeed(10000);                 // microstep below 10000 steps/s
-  board.setAcc(10000);                       // accelerate at 10000 steps/s/s
+  board.configSyncPin(BUSY_PIN, 0);       // BUSY pin low during operations;
+                                          //  second paramter ignored.
+  board.configStepMode(STEP_FS);          // 0 microsteps per step
+  board.setMaxSpeed(10000);               // 10000 steps/s max
+  board.setFullSpeed(10000);              // microstep below 10000 steps/s
+  board.setAcc(10000);                    // accelerate at 10000 steps/s/s
   board.setDec(10000);
-  board.setSlewRate(SR_530V_us);             // Upping the edge speed increases torque.
-  board.setOCThreshold(OC_750mA);            // OC threshold 750mA
-  board.setPWMFreq(PWM_DIV_2, PWM_MUL_2);    // 31.25kHz PWM freq
-  board.setOCShutdown(OC_SD_DISABLE);        // don't shutdown on OC
-  board.setVoltageComp(VS_COMP_DISABLE);     // don't compensate for motor V
-  board.setSwitchMode(SW_USER);              // Switch is not hard stop
-  board.setOscMode(EXT_16MHZ_OSCOUT_INVERT); // for boardA, we want 16MHz
-                                             //  external osc, 16MHz out. boardB
-                                             //  will be the same in all respects
-                                             //  but this, as it will generate the
-                                             //  clock.
-  board.setAccKVAL(128);                     // We'll tinker with these later, if needed.
+  board.setSlewRate(SR_530V_us);          // Upping the edge speed increases torque.
+  board.setOCThreshold(OC_750mA);         // OC threshold 750mA
+  board.setPWMFreq(PWM_DIV_2, PWM_MUL_2); // 31.25kHz PWM freq
+  board.setOCShutdown(OC_SD_DISABLE);     // don't shutdown on OC
+  board.setVoltageComp(VS_COMP_DISABLE);  // don't compensate for motor V
+  board.setSwitchMode(SW_USER);           // Switch is not hard stop
+  board.setOscMode(INT_16MHZ);            // for boardA, we want 16MHz
+                                          //  external osc, 16MHz out. boardB
+                                          //  will be the same in all respects
+                                          //  but this, as it will generate the
+                                          //  clock.
+  board.setAccKVAL(128);                  // We'll tinker with these later, if needed.
   board.setDecKVAL(128);
   board.setRunKVAL(128);
-  board.setHoldKVAL(32);                     // This controls the holding current; keep it low.
+  board.setHoldKVAL(32);                  // This controls the holding current; keep it low.
 }
