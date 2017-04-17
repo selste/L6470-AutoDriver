726376f733c6ddc9fabbffddf3e95ada14b23fec
Steffen Elste <steffen.elste@gmail.com>
2017-04-17 11:37:54 +0200

    Simplified copy of the 'SparkFunLibraryTest' project added that uses just one AutoDriver board

---
+++ Libraries/Arduino/examples/SparkFunLibrarySimplifiedTest/SparkFunLibrarySimplifiedTest.ino
@@ -0,0 +1,47 @@
+#include <SparkFunAutoDriver.h>
+#include <SPI.h>
+#include "SparkFunnotes.h"
+
+// Test sketch for the L6470 AutoDriver library. This program instantiates one
+//  AutoDriver board and uses it to play Jonathon Coulton's "Want You Gone" from
+//  the Portal 2 soundtrack. In a more general sense, it adds support for playing
+//  music with stepper motors. Not all notes can be played, of course- at too high
+//  a steps/sec rate, the motors will slip and dogs and cats will live together.
+
+#define CS_PIN 10
+#define RESET_PIN 6
+
+// Create our AutoDriver instance. The parameters are the position in the chain of
+//  boards (with board 0 being located at the end of the chain, farthest from the
+//  controlling processor), CS pin, and Reset pin.
+AutoDriver board(0, CS_PIN, RESET_PIN);
+
+void setup() {
+  Serial.begin(9600);
+  Serial.println("Hello world");
+  // Start by setting up the SPI port and pins. The
+  //  Autodriver library does not do this for you!
+  pinMode(RESET_PIN, OUTPUT);
+  pinMode(MOSI, OUTPUT);
+  pinMode(MISO, INPUT);
+  pinMode(SCK, OUTPUT);
+  pinMode(CS_PIN, OUTPUT);
+  digitalWrite(CS_PIN, HIGH);
+  digitalWrite(RESET_PIN, LOW);       // This low/high is a reset of the L6470 chip on the
+  digitalWrite(RESET_PIN, HIGH);      //  Autodriver board, and is a good thing to do at
+                                      //  the start of any Autodriver sketch, to be sure
+                                      //  you're starting the Autodriver from a known state.
+  SPI.begin();
+  SPI.setDataMode(SPI_MODE3);
+  dSPINConfig();
+}
+
+// loop() waits for a character- any character- and then plays the song.
+void loop() {
+  if (0!=Serial.available()) {
+    Serial.read();
+    Serial.println("Play it!");
+    wantYouGone();
+    Serial.println("Done playing!");
+  }
+}
---
+++ Libraries/Arduino/examples/SparkFunLibrarySimplifiedTest/SparkFunSupportFunctions.ino
@@ -0,0 +1,52 @@
+// Support functions.
+
+#define NOTE_DIVISOR 2  // My cheesy way of reducing the note frequencies to a range
+                        //  that doesn't cause the motor to slip. I *could* rewrite
+                        //  the wantYouGone() function to change the notes, but that
+                        //  would be a lot of work.
+
+int stepDir = 1;        // Direction flipping bit. Rather than all going one way,
+                        //  they change directions. It looks cooler.
+
+// To play a note, we start the motor spinning at the note's frequency in steps/s.
+//  The run() function automagically calculates the appropriate value to feed to the
+//  dSPIN part based on the desired steps/s.
+void playNote(int note, int duration) {
+  if (stepDir == 1)  board.run(FWD, note/NOTE_DIVISOR);
+  else               board.run(REV, note/NOTE_DIVISOR);
+  delay(duration);
+  stepDir*=-1;
+  board.softStop();
+  while (board.busyCheck());
+}
+
+// This is the configuration function for the dSPIN parts. Read the inline
+//  comments for more info.
+void dSPINConfig(void) {
+  board.SPIPortConnect(&SPI);                // Before doing anything else, we need to
+                                             //  tell the objects which SPI port to use.
+                                             //  Some devices may have more than one.
+
+  board.configSyncPin(BUSY_PIN, 0);          // BUSY pin low during operations;
+                                             //  second paramter ignored.
+  board.configStepMode(STEP_FS);             // 0 microsteps per step
+  board.setMaxSpeed(10000);                  // 10000 steps/s max
+  board.setFullSpeed(10000);                 // microstep below 10000 steps/s
+  board.setAcc(10000);                       // accelerate at 10000 steps/s/s
+  board.setDec(10000);
+  board.setSlewRate(SR_530V_us);             // Upping the edge speed increases torque.
+  board.setOCThreshold(OC_750mA);            // OC threshold 750mA
+  board.setPWMFreq(PWM_DIV_2, PWM_MUL_2);    // 31.25kHz PWM freq
+  board.setOCShutdown(OC_SD_DISABLE);        // don't shutdown on OC
+  board.setVoltageComp(VS_COMP_DISABLE);     // don't compensate for motor V
+  board.setSwitchMode(SW_USER);              // Switch is not hard stop
+  board.setOscMode(EXT_16MHZ_OSCOUT_INVERT); // for boardA, we want 16MHz
+                                             //  external osc, 16MHz out. boardB
+                                             //  will be the same in all respects
+                                             //  but this, as it will generate the
+                                             //  clock.
+  board.setAccKVAL(128);                     // We'll tinker with these later, if needed.
+  board.setDecKVAL(128);
+  board.setRunKVAL(128);
+  board.setHoldKVAL(32);                     // This controls the holding current; keep it low.
+}
